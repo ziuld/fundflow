c16568ec990d2c054b5abf04ec5903d1791d53c1
Luis Hernandez <hernandezldm@gmail.com>
2026-04-22 20:33:45 +0200

    test 2

---
+++ .github/workflows/scripts/trigger_github_workflow.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+set -eu
+
+WORKFLOW_ID="254786445"
+REF="${REF:-ci-cloudbuild-test}"
+
+echo "[INFO] Triggering GitHub workflow..."
+
+curl -sS --fail-with-body \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Content-Type: application/json" \
+  "https://api.github.com/repos/ziuld/fundflow/actions/workflows/${WORKFLOW_ID}/dispatches" \
+  --data "{\"ref\":\"${REF}\",\"inputs\":{\"source\":\"cloudbuild\",\"commit\":\"${COMMIT_SHA:-none}\"}}"
+
+echo "[OK] Workflow triggered successfully"
